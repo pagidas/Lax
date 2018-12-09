2476a4fdbab7dda4a9d2e0fbfed064759edabe51
pagidas <pagakrivos@gmail.com>
2018-12-09 15:02:41 +0200

    Added a constructor which will call another method to create the member in a database

--- java/src/Member.java
+++ java/src/Member.java
@@ -2,13 +2,19 @@ public class Member {
 
     int id;
     private String fullname, username, password, memberRole;
-    public static final String roles[] = {"Admin", "Manager", "Designer", "Developer"};
+    public static final String roles[] = {"Manager", "Designer", "Developer"};
 
-    public Member(String fullname, String username, String password, String memberRole) {
+    /*
+     *  Parameterized constructor to create a Member, calling
+     *  another method from Database class which executes the
+     *  query with the parameters given.
+     */
+    public Member(Database aDB, String fullname, String username, String password, String memberRole) {
         this.fullname = fullname;
         this.username = username;
         this.password = password;
         this.memberRole = memberRole;
+        this.id = aDB.createMember(fullname, username, password, memberRole);
     }
 
     //getters
