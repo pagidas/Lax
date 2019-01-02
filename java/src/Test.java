94ee708cae4f98ff5cb9df3cab8ec324c8b51ff0
Konstantinos Akrivos <pagakrivos@gmail.com>
2019-01-02 20:38:58 +0200

    Cleaning Test.java

--- java/src/Test.java
+++ java/src/Test.java
@@ -3,10 +3,6 @@ import login.Login;
 public class Test {
 
     public static void main(String[] args) {
-
-//        AdminView adminView = new AdminView().createTheMenu();
-//        adminView.displayMenu();
-
         Login.displayLogin();
     }
 }
